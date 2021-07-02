422bb9eaa88e700c12b96cbfe0e6cb504d588e92
Eric Gitonga <gitonga@gmail.com>
2021-07-02 14:21:20 +0300

    display altered image on streamlit

--- splat.py
+++ splat.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 import cv2
 
-image_file = st.file_uploader("Upload an image file", type=["jpg","png"])
+image_file = st.file_uploader("Upload an image file", type=["jpg","png","tif"])
 
 if image_file is not None:
     image_file = Image.open(image_file)
@@ -17,19 +17,31 @@ if image_file is not None:
         dimensions = (width,height)
 
         clear_image = cv2.resize(clear_image, dimensions)
-   
-    st.image(clear_image)
-    st.write(clear_image.shape[2])
+    if st.checkbox("Click to show image"):   
+        st.image(clear_image)
     
+    ci = clear_image.copy()
+    for r in range(ci.shape[0]):
+        for c in range(ci.shape[1]):
+            val0 = ci[r,c,0]
+            val1 = ci[r,c,1]
+            val2 = ci[r,c,2]
+            ci[r,c] = (val0,val0,val0)
+    
+    st.image(ci)
+            
     title_left, title_center, title_right = st.beta_columns((3))
     
     with title_left:
         st.markdown("### Red Channel")
-        st.write(clear_image[0])
+        st.write(clear_image[0][0])
+        st.write(ci[0][0])
     with title_center:
         st.markdown("### Green Channel")
-        st.write(clear_image[1])
+        st.write(clear_image[1][0])
+        st.write(ci[1][0])
     with title_right:
         st.markdown("### Blue Channel")
-        st.write(clear_image[2])
+        st.write(clear_image[2][0])
+        st.write(ci[2][0])
     
