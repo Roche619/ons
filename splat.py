d29cd81c840f9e203eb1caa1c02f91c0c4ac0a49
Eric Gitonga <gitonga@gmail.com>
2021-07-02 20:18:14 +0300

    made more descriptive

--- splat.py
+++ splat.py
@@ -4,6 +4,8 @@ from PIL import Image
 import numpy as np
 import cv2
 
+st.title("Exploring the wonderful world of image manipulation with OpenCV")
+
 image_file = st.file_uploader("Upload an image file", type=["jpg","png","tif"])
 
 if image_file is not None:
@@ -17,31 +19,34 @@ if image_file is not None:
         dimensions = (width,height)
 
         clear_image = cv2.resize(clear_image, dimensions)
-    if st.checkbox("Click to show image"):   
-        st.image(clear_image)
     
     ci = clear_image.copy()
     for r in range(ci.shape[0]):
         for c in range(ci.shape[1]):
-            val0 = ci[r,c,0]
-            val1 = ci[r,c,1]
-            val2 = ci[r,c,2]
-            ci[r,c] = (val0,val0,val0)
+            val0 = ci[c,r,0]
+            val1 = ci[c,r,1]
+            val2 = ci[c,r,2]
+            ci[r,c] = (val2,val2,val2)
     
-    st.image(ci)
-            
-    title_left, title_center, title_right = st.beta_columns((3))
-    
+    fly_left, fly_right = st.beta_columns(2)
+    with fly_left:
+        st.markdown("### Original image")
+        st.image(clear_image)
+    with fly_right:
+        st.markdown("### Processed image")
+        st.image(ci)
+
+    st.markdown("#### First few rows of RGB Channels of original image (top row) and processed image (bottom row)")
+
+
+    title_left, title_center, title_right = st.beta_columns(3)
     with title_left:
-        st.markdown("### Red Channel")
-        st.write(clear_image[0][0])
-        st.write(ci[0][0])
+        st.write(clear_image[0][:3])
+        st.write(ci[0][:3])
     with title_center:
-        st.markdown("### Green Channel")
-        st.write(clear_image[1][0])
-        st.write(ci[1][0])
+        st.write(clear_image[1][:3])
+        st.write(ci[1][:3])
     with title_right:
-        st.markdown("### Blue Channel")
-        st.write(clear_image[2][0])
-        st.write(ci[2][0])
+        st.write(clear_image[2][:3])
+        st.write(ci[2][:3])
     
