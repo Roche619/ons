a814ec917a36b78d08475c656351b04be59fc9f2
Eric Gitonga <gitonga@gmail.com>
2021-07-04 23:25:49 +0300

    add swap rgb channels

--- splat.py
+++ splat.py
@@ -42,12 +42,13 @@ if image_file is not None:
     resized_image = image_resize(input_image, sf1)
     
     effect = st.sidebar.selectbox("Select type of image manipulation you want to experiment with",
-         ["Reflect on Diagonal", "Edge Enhancement"])    
+         ["Reflect on Diagonal", "Edge Enhancement", "NumPy Generated Values", "Swap RGB Channels"])    
     
+    output_image = resized_image.copy()
+
     if effect == "Reflect on Diagonal":
         st.markdown("## Reflect on Diagonal")
 
-        output_image = resized_image.copy()
         dim = output_image.shape[0]
         output_image = output_image[:dim,:dim,:3]
         for r in range(output_image.shape[0]):
@@ -73,8 +74,6 @@ if image_file is not None:
         value_1 = st.sidebar.number_input("Input value 1", value=0, min_value=0, max_value=255)
         value_2 = st.sidebar.number_input("Input value 2", value=255, min_value=0, max_value=255)
     
-        output_image = resized_image.copy()
-
         for r in range(resized_image.shape[0]):
             for c in range(resized_image.shape[1]):
                 if resized_image.item((r,c,1)) >= threshold:
@@ -94,4 +93,98 @@ if image_file is not None:
                     output_image.itemset((r,c,0), resized_image.item((r1,c1,0)))
                     output_image.itemset((r,c,1), resized_image.item((r1,c1,1)))
                     output_image.itemset((r,c,2), resized_image.item((r1,c1,2)))
+        display_figures(resized_image, output_image)
+        
+        
+    elif effect == "NumPy Generated Values":
+        np_choice = st.sidebar.selectbox("Select the NumPy function to use", ["sum", "average", "maximum", "minimum"])
+        if np_choice == "sum":
+            numpy_func = np.sum
+        elif np_choice == "average":
+            numpy_func = np.mean
+        elif np_choice == "maximum":
+            numpy_func = np.max
+        elif np_choice == "minimum":
+            numpy_func = np.min
+            
+        for r in range(resized_image.shape[0]):
+            for c in range(resized_image.shape[1]):
+                if(is_outside(r, c, resized_image.shape)):
+                    output_image.itemset((r,c,0), 0)
+                    output_image.itemset((r,c,1), 0)
+                    output_image.itemset((r,c,2), 0)
+                else:
+                    output_image.itemset((r,c,0), numpy_func([resized_image.item((r,c,1)), resized_image.item((r,c,2))]))
+                    output_image.itemset((r,c,1), numpy_func([resized_image.item((r,c,0)), resized_image.item((r,c,2))]))
+                    output_image.itemset((r,c,2), numpy_func([resized_image.item((r,c,0)), resized_image.item((r,c,1))]))
+        display_figures(resized_image, output_image)
+        
+    elif effect == "Swap RGB Channels":
+        channel_choice = st.sidebar.select_slider("Select the channel value", list(range(1,28)))
+        if channel_choice == 1:
+            c1, c2, c3 = 0, 0, 0
+        elif channel_choice == 2:
+            c1, c2, c3 = 0, 0, 1
+        elif channel_choice == 3:
+            c1, c2, c3 = 0, 0, 2
+        elif channel_choice == 4:
+            c1, c2, c3 = 0, 1, 0
+        elif channel_choice == 5:
+            c1, c2, c3 = 0, 1, 1
+        elif channel_choice == 6:
+            c1, c2, c3 = 0, 1, 2
+        elif channel_choice == 7:
+            c1, c2, c3 = 0, 2, 0
+        elif channel_choice == 8:
+            c1, c2, c3 = 0, 2, 1
+        elif channel_choice == 9:
+            c1, c2, c3 = 0, 2, 2
+        elif channel_choice == 10:
+            c1, c2, c3 = 1, 0, 0
+        elif channel_choice == 11:
+            c1, c2, c3 = 1, 0, 1
+        elif channel_choice == 12:
+            c1, c2, c3 = 1, 0, 2
+        elif channel_choice == 13:
+            c1, c2, c3 = 1, 1, 0
+        elif channel_choice == 14:
+            c1, c2, c3 = 1, 1, 1
+        elif channel_choice == 15:
+            c1, c2, c3 = 1, 1, 2
+        elif channel_choice == 16:
+            c1, c2, c3 = 1, 2, 0
+        elif channel_choice == 17:
+            c1, c2, c3 = 1, 2, 1
+        elif channel_choice == 18:
+            c1, c2, c3 = 1, 2, 2
+        elif channel_choice == 19:
+            c1, c2, c3 = 2, 0, 0
+        elif channel_choice == 20:
+            c1, c2, c3 = 2, 0, 1
+        elif channel_choice == 21:
+            c1, c2, c3 = 2, 0, 2
+        elif channel_choice == 22:
+            c1, c2, c3 = 2, 1, 0
+        elif channel_choice == 23:
+            c1, c2, c3 = 2, 1, 1
+        elif channel_choice == 24:
+            c1, c2, c3 = 2, 1, 2
+        elif channel_choice == 25:
+            c1, c2, c3 = 2, 2, 0
+        elif channel_choice == 26:
+            c1, c2, c3 = 2, 2, 1
+        elif channel_choice == 27:
+            c1, c2, c3 = 2, 2, 2
+            
+        for r in range(output_image.shape[0]):
+            for c in range(output_image.shape[1]):
+                if(is_outside(r, c, resized_image.shape)):
+                    output_image.itemset((r,c,0), 0)
+                    output_image.itemset((r,c,1), 0)
+                    output_image.itemset((r,c,2), 0)
+                else:
+                    val_0 = output_image.item((r,c,c1))
+                    val_1 = output_image.item((r,c,c2))
+                    val_2 = output_image.item((r,c,c3))
+                output_image[r,c] = (val_0,val_1,val_2)
         display_figures(resized_image, output_image)
