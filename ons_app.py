1aed6d0b2998fdbfabc153cee2a7f182e46e8174
Eric Gitonga <gitonga@gmail.com>
2021-07-09 12:20:45 +0300

    first streamlit commit

---
+++ ons_app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 12:08:38 2021
+
+@author: Eric Gitonga
+"""
+import streamlit as st
+import streamlit_analytics
+import cv2
+import numpy as np
+
+
